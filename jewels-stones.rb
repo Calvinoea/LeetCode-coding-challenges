f857099f2ebd3a4953d173f45db0b0e35e295002
Calvinoea <39657008+Calvinoea@users.noreply.github.com>
2020-05-15 11:28:48 +0100

    Create jewels-stones.rb

---
+++ jewels-stones.rb
@@ -0,0 +1,14 @@
+# @param {String} j
+# @param {String} s
+# @return {Integer}
+def num_jewels_in_stones(j, s)
+    
+    # count the number of occurences of letters,  'j' in the string, s
+    
+    s.count j
+    
+    
+    
+    
+    
+end
